d4605a8ffd7e5258f13aa074244b2f081e7dd9ed
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-10-28 11:38:59 -0300

    use zsh

--- ubuntu_dev.sh
+++ ubuntu_dev.sh
@@ -1,3 +1,5 @@
+#!/bin/zsh
+
 # install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
