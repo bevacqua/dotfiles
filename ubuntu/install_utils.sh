69ea483d8fdb151f7dc0be96ba86af30fd1a90e7
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-12-30 14:26:44 -0300

    bitcoin steps

--- ubuntu/install_utils.sh
+++ ubuntu/install_utils.sh
@@ -32,3 +32,8 @@ sudo apt-get install vlc browser-plugin-vlc -y
 sudo add-apt-repository ppa:umang/indicator-stickynotes -y
 sudo apt-get update
 sudo apt-get install indicator-stickynotes
+
+# bitcoin
+# sudo add-apt-repository ppa:bitcoin/bitcoin -y
+# sudo apt-get update
+# sudo apt-get install bitcoin-qt -y
