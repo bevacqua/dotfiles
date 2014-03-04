1c82bfae7836206d56337ec39db05f0be4e19fe5
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-03-03 19:02:35 -0800

    create link to subl

--- osx/osx.sh
+++ osx/osx.sh
@@ -13,3 +13,5 @@ chsh -s /usr/local/bin/zsh $USER
 # PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 
 # take out ubuntu.sh stuff that's common into somewhere else, and use $PWD to source that stuff.
+
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
