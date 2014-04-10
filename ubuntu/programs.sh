e9fb61c38718c86fa5a71c94fc5889f0a807b9fd
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-04-10 17:55:48 -0300

    chrome from google's ppa

--- ubuntu/programs.sh
+++ ubuntu/programs.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
 # chrome
-sudo apt-get install libxss1
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome*.deb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-beta
 
 # hipchat
 echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
