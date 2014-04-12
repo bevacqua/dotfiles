15f664096df0223cebd263ffbaf01eabc6f32572
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-04-12 14:20:19 -0300

    mongodb on startup always

--- osx/dev.sh
+++ osx/dev.sh
@@ -3,6 +3,10 @@
 # install mongodb
 brew install mongodb
 
+# mongodb-as-a-service on startup and right now
+ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+
 # install heroku toolbelt
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
