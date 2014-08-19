18f0f14717f2b183adee49cad106f17c0115be56
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-08-18 21:51:34 -0300

    stuff

--- osx/dev.zsh
+++ osx/dev.zsh
@@ -24,3 +24,9 @@ brew install phantomjs
 
 # gradle
 brew install gradle
+
+# pkg-config
+curl http://pkgconfig.freedesktop.org/releases/pkg-config-0.23.tar.gz -o pkgconfig.tgz
+tar -zxf pkgconfig.tgz && cd pkg-config-0.23
+./configure && make install
+cd .. && rm -rf pkg-config-0.23 && rm pkgconfig.tgz
