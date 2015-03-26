012447e13e0b82f77e21e132079384b39ba08228
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-03-26 14:50:45 -0300

    moved brew installer

--- osx/osx.zsh
+++ osx/osx.zsh
@@ -4,7 +4,7 @@
 cp $PWD/osx/support/latinamerican.keylayout ~/Library/Keyboard\ Layouts
 
 # install brew
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # cask for brew
 brew tap phinze/cask
