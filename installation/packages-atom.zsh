aa8ef7c3c96ff46f11bf3eaedc5f6e8b7e31f211
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-07-19 12:17:32 -0700

    reasonable packages.

--- installation/packages-atom.zsh
+++ installation/packages-atom.zsh
@@ -12,11 +12,11 @@ apm i sort-lines
 
 # linting
 apm i linter
-apm i linter-htmllint
 apm i linter-csslint
 apm i linter-eslint
 
 # syntax highlighting
+apm i stylus
 apm i language-dots
 apm i language-babel
 
@@ -26,9 +26,7 @@ apm i merge-conflicts
 apm i rest-client
 
 # general
-apm i project-plus
 apm i open-recent
 apm i wordcount
 apm i minimap
 apm i minimap-highlight-selected
-apm i file-icons
