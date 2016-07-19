310e1b3be9d35a51111613244180bbaecab56cba
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-07-19 10:42:13 -0700

    monospace font and less comments

--- hyperterm/.hyperterm.js
+++ hyperterm/.hyperterm.js
@@ -1,65 +1,38 @@
 module.exports = {
   config: {
-    // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14.5,
+    fontFamily: '"Droid Sans Mono for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
-    // font family with optional fallbacks
-    fontFamily: '"Meslo LG M for Powerline", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    css: '', // custom css to embed in the main window
+    termCSS: '', // custom css to embed in the terminal window
 
-    // terminal cursor background color (hex)
-    cursorColor: '#e92c6c',
-
-    // color of the text
-    foregroundColor: '#839496',
-
-    // terminal background color
-    backgroundColor: '#002b36',
-
-    // border color (window, tabs)
-    borderColor: 'rgba(255,255,255,.1)',
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: '',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
+    cursorColor: '#e92c6c',
+    foregroundColor: '#839496',
+    backgroundColor: '#002b36',
+    borderColor: 'rgba(255,255,255,.1)',
+
     colors: [
-      '#002b36', // background
-      '#dc322f', // red
-      '#859900', // green
-      '#b58900', // yellow
-      '#268bd2', // blue
-      '#6c71c4', // violet
-      '#2aa198', // cyan
-      '#657b83', // light gray
-      '#586e75', // medium gray
-      '#dc322f', // red
-      '#859900', // green
-      '#b58900', // yellow
-      '#268bd2', // blue
-      '#6c71c4', // violet
-      '#2aa198', // cyan
-      '#ffffff', // white
+      '#002b36',
+      '#dc322f',
+      '#859900',
+      '#b58900',
+      '#268bd2',
+      '#6c71c4',
+      '#2aa198',
+      '#657b83',
+      '#586e75',
+      '#dc322f',
+      '#859900',
+      '#b58900',
+      '#268bd2',
+      '#6c71c4',
+      '#2aa198',
+      '#ffffff'
     ]
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: [
-  ],
-
-  // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
+  plugins: [],
   localPlugins: []
 };
