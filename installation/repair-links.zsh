d0e030e0d0df2f8cf4e0dbb0918bd1586f7a5eec
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2018-04-26 00:50:01 -0300

    tweaks

--- installation/repair-links.zsh
+++ installation/repair-links.zsh
@@ -5,6 +5,7 @@ echo "[global] Creating symbolic links on directory: $PWD"
 # repair symbolic links
 ln -sfn $PWD/zsh/agnork.zsh-theme ~/.oh-my-zsh/themes/agnork.zsh-theme
 ln -sfn $PWD/zsh/config ~/.zshrc
+ln -sfn $PWD/zsh/profile ~/.zprofile
 ln -sfn $PWD/ssh/config ~/.ssh/config
 
 # OS-specific symbolic links
